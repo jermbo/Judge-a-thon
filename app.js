8b8232a86563400547f6aeb41bd1c32d73f4cb33
jlawson <jlawson@hugeinc.com>
2018-08-08 16:38:16 -0400

    setting up local dev

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));
