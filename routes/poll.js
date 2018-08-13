a8c4d97277f1b1003c847049b51a069fdded26fa
jlawson <jlawson@hugeinc.com>
2018-08-13 16:12:05 -0400

    finalizing poll app

--- routes/poll.js
+++ routes/poll.js
@@ -1,29 +1,43 @@
+require("dotenv/config");
 const express = require("express");
 const router = express.Router();
-
+const Vote = require("../models/Vote");
 const Pusher = require("pusher");
 
-var pusher = new Pusher({
-    appId: "574940",
-    key: "c8fd2a38c6a9b084ccde",
-    secret: "36b23fff87c72e17640d",
+// import dotenv from "dotenv";
+// import express from "express";
+// import Vote from "../models/Vote";
+// import Pusher from "Pusher";
+// const router = express.Router();
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
     cluster: "us2",
     encrypted: true
 });
 
 router.get("/", (req, res) => {
-    res.send("POLL");
+    Vote.find().then(votes => res.json({ success: true, votes: votes }));
 });
 
 router.post("/", (req, res) => {
-    pusher.trigger("judgeathon", "judge", {
-        points: 1,
-        os: req.body.os
-    });
+    const newVote = {
+        os: req.body.os,
+        points: 1
+    };
 
-    return res.json({
-        success: true,
-        message: "Thanks for your vote"
+    new Vote(newVote).save().then(vote => {
+        pusher.trigger("judgeathon", "judge", {
+            points: parseInt(vote.points),
+            os: vote.os
+        });
+
+        return res.json({
+            success: true,
+            message: "Thanks for your vote"
+        });
     });
 });
 
